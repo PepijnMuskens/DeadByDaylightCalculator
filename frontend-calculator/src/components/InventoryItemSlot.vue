d98fcfd48ef66b59e1d3f8df10345485ed9bb3c7
Pepijn <437675@student.fontys.nl>
2023-01-11 18:54:56 +0100

    update background info hover

--- frontend-calculator/src/components/InventoryItemSlot.vue
+++ frontend-calculator/src/components/InventoryItemSlot.vue
@@ -3,7 +3,20 @@
   <div v-if="hover" class="info">
     <h3>{{item.name}}</h3>
     <p>{{item.charges}} charges</p>
-    <p>{{item.selfHealSpeed * 100}}% healspeed</p>
+    <p v-if="item.selfHealSpeed != null">{{item.selfHealSpeed * 100}}% self healspeed</p>
+    <p v-if="item.selfHealSpeed != null">{{item.selfHealSpeed * 100}}% self healspeed</p>
+
+    <p v-if="item.repairSpeed != null">{{(item.repairSpeed * 100).toFixed(0)}}% repair speed</p>
+    <p v-if="item.saboSpeed != null">{{item.saboSpeed}} c/s sabotage speed</p>
+
+    <p v-if="item.consumptionRate != 1">consumptionsrate {{item.consumptionRate}}</p>
+    <p v-if="item.beamRange != null">range: {{item.beamRange}} meter</p>
+    <p v-if="item.beamAngle != null">{{item.beamAngle}}° beam width</p>
+    <p v-if="item.blindDuration != null">{{item.blindDuration}} seconds blind duration</p>
+
+    <p v-if="item.aurarange > 0">aura range: {{item.aurarange}} meters</p>
+
+    
   </div>
 </template> 
 
@@ -35,10 +48,9 @@ export default {
 }
 .info{
   position: absolute;
-  border-style: solid;
-  width: 10%;
-  height: 20%;
-  background-color: gray;
+  width: 15%;
+  padding-bottom: 1%;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.85),rgba(0, 0, 0, 0.6));
   margin-left: 6%;
 }
 </style>
